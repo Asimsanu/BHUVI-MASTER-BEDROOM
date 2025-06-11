45b2598df25bbc8ade6536410dd871f3fad2caf7
Asimsanu <asimsanu@studioemfiz.com>
2025-06-11 12:13:10 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,49 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-mbr",
+      "name": "mbr",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "yaw": 0.4453706747950008,
+        "pitch": 0.15863225916115908,
+        "fov": 1.6126458636171515
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -2.115881983669045,
+          "pitch": 0.4289162162336293,
+          "title": "Title",
+          "text": "Text"
+        }
+      ]
+    }
+  ],
+  "name": "MASTER BEDROOM",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
